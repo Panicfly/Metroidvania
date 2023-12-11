ae56f39648059b83c8ba9a8fc0a5bbcc6caf0291
Panicfly <37988989+Panicfly@users.noreply.github.com>
2023-12-11 22:26:25 +0100

    Add Player Blaster & Blaster Rotation & Backwards Movement Animation

--- player/player.gd
+++ player/player.gd
@@ -13,15 +13,18 @@ const DUST_EFFECT_SCENE = preload("res://effects/dust_effect.tscn")
 @onready var animation_player = $AnimationPlayer
 @onready var sprite_2d = $Sprite2D
 @onready var coyote_jump_timer = $CoyoteJumpTimer
+@onready var player_blaster = $PlayerBlaster
 
 func _physics_process(delta):
 	apply_gravity(delta)
-	var input_axis = Input.get_axis("ui_left", "ui_right")
+	var input_axis = Input.get_axis("move_left", "move_right")
 	if is_moving(input_axis):
 		apply_accelaration(delta, input_axis)
 	else:
 		apply_friction(delta)
 	jump_check()
+	if Input.is_action_just_pressed("fire_bullet"):
+		player_blaster.fire_bullet()
 	update_animations(input_axis)
 	var was_on_floor = is_on_floor()
 	move_and_slide()
@@ -50,16 +53,22 @@ func apply_friction(delta):
 
 func jump_check():
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
-		if Input.is_action_just_pressed("ui_up"):
+		if Input.is_action_just_pressed("jump"):
 			velocity.y = -jump_force
 	if not is_on_floor():
-		if Input.is_action_just_released("ui_up") and velocity.y < -jump_force / 2:
+		if Input.is_action_just_released("jump") and velocity.y < -jump_force / 2:
 			velocity.y = -jump_force / 2
 
 func update_animations(input_axis):
+	sprite_2d.scale.x = sign(get_local_mouse_position().x)
+	#You need this check because if the mouse is in the origin the character despawns
+	if abs(sprite_2d.scale.x) != 1:
+		sprite_2d.scale.x = 1
 	if is_moving(input_axis):
 		animation_player.play("run")
-		sprite_2d.scale.x = sign(input_axis)
+		#The character was able to move backwards without the speed_scale change. 
+		#When you face to the right and walk backwards it's -1
+		animation_player.speed_scale = input_axis * sprite_2d.scale.x
 	else:
 		animation_player.play("idle")
 	
