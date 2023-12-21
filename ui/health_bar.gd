9109772f88a2649cd38fd698bd5fae2b62852756
Panicfly <37988989+Panicfly@users.noreply.github.com>
2023-12-22 00:48:39 +0100

    Add IFrame Blinking Animation + Fixed Health Bar UI

--- ui/health_bar.gd
+++ ui/health_bar.gd
@@ -4,6 +4,8 @@ extends Control
 @onready var full = $Full
 
 func _ready():
+	update_health_bar()
+	update_max_health_bar()
 	PlayerStats.health_changed.connect(update_health_bar)
 	PlayerStats.max_health_changed.connect(update_max_health_bar)
 
