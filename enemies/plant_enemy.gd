199725db9c2d357f63904233033c8157eedc8af4
Panicfly <37988989+Panicfly@users.noreply.github.com>
2023-12-25 23:53:33 +0100

    Improve Enemy Bullet Velocity and Rotation Settings

--- enemies/plant_enemy.gd
+++ enemies/plant_enemy.gd
@@ -12,10 +12,13 @@ const ENEMY_BULLET_SCENE = preload("res://enemies/enemy_bullet.tscn")
 func fire_bullet():
 	var bullet = Utils.instantiate_scene_on_world(ENEMY_BULLET_SCENE, bullet_spawn_point.global_position) as Projectile
 	var direction = global_position.direction_to(fire_direction.global_position)
-	bullet.rotation = direction.angle()
-	bullet.rotate(randf_range(-deg_to_rad(spread / 2), deg_to_rad(spread / 2)))
-	bullet.speed = bullet_speed
-	bullet.update_velocity()
+	var velocity = direction.normalized() * bullet_speed
+	velocity = velocity.rotated(randf_range(-deg_to_rad(spread / 2), deg_to_rad(spread / 2)))
+	bullet.velocity = velocity
+	#bullet.rotation = direction.angle()
+	#bullet.rotate(randf_range(-deg_to_rad(spread / 2), deg_to_rad(spread / 2)))
+	#bullet.speed = bullet_speed
+	#bullet.update_velocity()
 
 func _on_hurtbox_hurt(_hitbox, damage):
 	stats.health -= damage
