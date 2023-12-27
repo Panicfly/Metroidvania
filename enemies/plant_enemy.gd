601deae9fbc7fa898fceb83ceb34a8f591345940
Panicfly <37988989+Panicfly@users.noreply.github.com>
2023-12-27 23:42:40 +0100

    Add Enemy Death Effect

--- enemies/plant_enemy.gd
+++ enemies/plant_enemy.gd
@@ -1,18 +1,21 @@
 extends Node2D
 
 const ENEMY_BULLET_SCENE = preload("res://enemies/enemy_bullet.tscn")
+const ENEMY_DEATH_EFFECT_SCENE = preload("res://effects/enemy_death_effect.tscn")
 
 @export var bullet_speed = 30
-@export var spread = 30
+@export var spread = 45
 
 @onready var stats = $Stats
 @onready var bullet_spawn_point = $BulletSpawnPoint
 @onready var fire_direction = $FireDirection
+@onready var death_effect_location = $DeathEffectLocation
 
 func fire_bullet():
 	var bullet = Utils.instantiate_scene_on_world(ENEMY_BULLET_SCENE, bullet_spawn_point.global_position) as Projectile
 	var direction = global_position.direction_to(fire_direction.global_position)
 	var velocity = direction.normalized() * bullet_speed
+	@warning_ignore("integer_division")
 	velocity = velocity.rotated(randf_range(-deg_to_rad(spread / 2), deg_to_rad(spread / 2)))
 	bullet.velocity = velocity
 	#bullet.rotation = direction.angle()
@@ -24,4 +27,5 @@ func _on_hurtbox_hurt(_hitbox, damage):
 	stats.health -= damage
 	
 func _on_stats_no_health():
+	Utils.instantiate_scene_on_world(ENEMY_DEATH_EFFECT_SCENE, death_effect_location.global_position)
 	queue_free()
--- enemies/walking_enemy.gd
+++ enemies/walking_enemy.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+const ENEMY_DEATH_EFFECT_SCENE = preload("res://effects/enemy_death_effect.tscn")
+
 @export var speed = 40.0
 @export var turns_at_ledges = true
 
@@ -8,6 +10,7 @@ var direction = 1.0
 @onready var sprite_2d = $Sprite2D
 @onready var floor_cast = $FloorCast
 @onready var stats = $Stats
+@onready var death_effect_location = $DeathEffectLocation
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -36,4 +39,5 @@ func _on_hurtbox_hurt(_hitbox, damage):
 	stats.health -= damage
 
 func _on_stats_no_health():
+	Utils.instantiate_scene_on_world(ENEMY_DEATH_EFFECT_SCENE, death_effect_location.global_position)
 	queue_free()
