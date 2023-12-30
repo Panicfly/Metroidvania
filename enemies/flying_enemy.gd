c29a386eb6673a554211823a547d70966b763cff
Panicfly <37988989+Panicfly@users.noreply.github.com>
2023-12-30 23:55:49 +0100

    Add simple Player Targeting for Enemies

--- enemies/flying_enemy.gd
+++ enemies/flying_enemy.gd
@@ -2,9 +2,13 @@ extends CharacterBody2D
 
 @export var acceleration = 100
 @export var max_speed = 40
+@export var player_path : NodePath
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _physics_process(delta):
-	
+	if player_path is NodePath:
+		var player = get_node(player_path)
+		if player is CharacterBody2D:
+			move_toward_target(player.global_position)
 	move_and_slide()
