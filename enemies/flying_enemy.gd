631c0af1a55e03fa2928fcbe2ff0b57a43604048
Panicfly <37988989+Panicfly@users.noreply.github.com>
2024-01-01 23:57:15 +0100

    Improve Enemy Pathfinding and change Flying Enemy behaviour

--- enemies/flying_enemy.gd
+++ enemies/flying_enemy.gd
@@ -7,11 +7,16 @@ const ENEMY_DEATH_EFFECT_SCENE = preload("res://effects/enemy_death_effect.tscn"
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var stats = $Stats
+@onready var waypoint_pathfinding = $WaypointPathfinding
+@onready var visible_on_screen_notifier_2d = $VisibleOnScreenNotifier2D
+
+func _ready():
+	set_physics_process(false)
 
 func _physics_process(delta):
 	var player = MainInstances.player
 	if player is CharacterBody2D:
-		move_toward_position(player.global_position, delta)
+		move_toward_position(waypoint_pathfinding.pathfinding_next_postition, delta)
 
 func move_toward_position(target_position, delta):
 	var direction = global_position.direction_to(target_position)
@@ -25,3 +30,6 @@ func _on_hurtbox_hurt(_hitbox, damage):
 func _on_stats_no_health():
 	Utils.instantiate_scene_on_world(ENEMY_DEATH_EFFECT_SCENE, global_position)
 	queue_free()
+
+func _on_visible_on_screen_notifier_2d_screen_entered():
+	set_physics_process(true)
