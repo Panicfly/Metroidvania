c26290b02385c6ebe3f49934d577349271667789
Panicfly <37988989+Panicfly@users.noreply.github.com>
2024-01-10 20:14:03 +0100

    Door Bugfix for 2 doors which are at the same position in different levels

--- world/door.gd
+++ world/door.gd
@@ -1,6 +1,8 @@
 class_name Door
 extends Area2D
 
+var active = false
+
 @export_file("*.tscn") var new_level_path
 @export var connection : DoorConnection
 
@@ -16,7 +18,9 @@ func get_direction():
 
 func _physics_process(_delta):
 	var player = MainInstances.player as Player
-	if not player is Player : return
+	if not player is Player: return
+	#Prevents a bug where you skip a level when 2 doors are on the same position
+	if not active: return
 	if overlaps_body(player) and new_level_path and Input.is_action_just_pressed("up"):
 		#var player_access_direction = sign(player.velocity.x)
 		#var direction = get_direction()
@@ -24,3 +28,6 @@ func _physics_process(_delta):
 		#if player_access_direction == direction:
 			#print("Door entered")
 			#Events.door_entered.emit(self)
+
+func _on_timer_timeout():
+	active = true
